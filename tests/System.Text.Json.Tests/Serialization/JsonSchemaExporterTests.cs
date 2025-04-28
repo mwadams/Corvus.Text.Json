73fe899447ede678dfc1600538023e498e938ba6
Matthew Adams <matthew.adams@endjin.com>
2025-04-28 11:01:17 +0100

    Add project files.

---
+++ tests/System.Text.Json.Tests/Serialization/JsonSchemaExporterTests.cs
@@ -0,0 +1,9 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Corvus.Text.Json.Serialization.Tests;
+
+namespace Corvus.Text.Json.Schema.Tests
+{
+    public sealed class JsonSchemaExporterTests_Reflection() : JsonSchemaExporterTests(JsonSerializerWrapper.StringSerializer);
+}
